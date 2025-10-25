c7cf0848f47ef9b98aa17e5708a2698462072b2d
CBlair <cblair2782@gmail.com>
2025-10-25 01:17:00 -0500

    Added copy.c

---
+++ copy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	int n, i;
+	printf("Enter number of elements: ");
+	scanf("%d", &n);
+	int arr1[n], arr2[n];
+	printf("Enter %d integers for first array:\n", n);
+	for (i = 0; i < n; i++) {
+		scanf("%d", &arr1[i]);
+	}
+	for (i = 0; i < n; i++) {
+		arr2[i] = arr1[i];
+	}
+	printf("Copied Array\n");
+	for (i = 0; i < n; i++) {
+		printf("%d", arr2[i]);
+	}
+	return 0;
+}
