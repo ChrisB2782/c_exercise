60b24ec8de492e2bccde7f6003085942fe62abe5
CBlair <cblair2782@gmail.com>
2025-11-10 18:07:36 -0600

    Added function exercises

---
+++ function1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	int x = 27;
+    	float y = (float) x;
+    	printf("%d\n", x);
+    	printf("%f\n", y);
+    	return 0;
+}
---
+++ function2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	double d = 9.78;
+    	int i = (int) d;
+    	printf("%f\n", d);
+    	printf("%d\n", i);
+    	return 0;
+}
